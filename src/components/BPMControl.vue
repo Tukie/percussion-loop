4e29c43b2f924d2e111363473c0ec1e1d9090aec
tukie <sadawutsunee.2885@gmal.com>
2026-03-22 13:08:45 +0700

    chore: adjust style of bpm

--- src/components/BPMControl.vue
+++ src/components/BPMControl.vue
@@ -9,9 +9,7 @@ const model = defineModel('bpm');
   <div class="flex flex-col xl:flex-row items-center justify-center gap-10 w-full">
     <div class="flex flex-col sm:flex-row items-center gap-4 w-full justify-center">
       <label for="bpm-input" class="block text-sm font-medium text-gray-400 mb-2">BPM</label>
-      <input type="number" id="bpm-input" step="1" min="10" max="300"
-        class="w-full max-w-[100px] bg-gray-700 border border-gray-600 text-white p-2 rounded-lg focus:ring-blue-500 focus:border-blue-500 text-center"
-        :value="model" @change="model = $event.target.value" />
+      <span class="text-4xl font-bold text-white">{{ model }}</span>
       <div class="flex items-center gap-5 w-full">
         <Button @click="model -= 1" label="-" class="shrink-0 px-12!"></Button>
         <Slider v-model="model" :min="50" :max="200" class="w-full" :pt="{ handle: { class: 'size-9! bg-white!' } }" />
@@ -23,7 +21,6 @@ const model = defineModel('bpm');
     <div class="w-full flex overflow-x-auto sm:grid grid-cols-4 md:grid-cols-7 gap-4">
       <Button class="shrink-0 p-3!" @click="model = 70">70</Button>
       <Button class="shrink-0 p-3!" @click="model = 90">90</Button>
-      <Button class="shrink-0 p-3!" @click="model = 120">120</Button>
       <Button class="shrink-0 p-3!" @click="model = 136">136</Button>
       <Button class="shrink-0 p-3!" @click="model = 142">142</Button>
       <Button class="shrink-0 p-3!" @click="model = 155">155</Button>
